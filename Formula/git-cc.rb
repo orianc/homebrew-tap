0ccbf55411bc500eb6a7b6451039b0564af66460
Orian Cros <contact@oriancros.com>
2025-06-17 02:29:41 +0200

    feat: add git-cc v0.1.0

--- Formula/git-cc.rb
+++ Formula/git-cc.rb
@@ -1,8 +1,8 @@
 class GitSpeed < Formula
   desc "Interactive conventional Git commit tool"
-  homepage "https://github.com/orianc/git-speed"
-  url "https://github.com/orianc/git-speed/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "VOTRE_SHA256"
+  homepage "https://github.com/orianc/git-cc"
+  url "https://github.com/orianc/git-cc/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c7282d09cfda8cfc250ed4d8cdfbea0434e2d93f40a651fbdaecefc170242845"
 
   depends_on "gum"
 
