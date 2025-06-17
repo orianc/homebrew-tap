5f3a8e436cdf55f7dcceea6bead7c998df22a31e
Orian Cros <contact@oriancros.com>
2025-06-17 02:41:30 +0200

    chore: update pkg name

--- Formula/git-cc.rb
+++ Formula/git-cc.rb
@@ -1,17 +1,17 @@
 class GitSpeed < Formula
   desc "Interactive conventional Git commit tool"
-  homepage "https://github.com/orianc/git-cc"
-  url "https://github.com/orianc/git-cc/archive/refs/tags/v0.1.0.tar.gz"
+  homepage "https://github.com/orianc/git-speed"
+  url "https://github.com/orianc/git-speed/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "c7282d09cfda8cfc250ed4d8cdfbea0434e2d93f40a651fbdaecefc170242845"
 
   depends_on "gum"
 
   def install
-    bin.install "bin/gitc"
-    man1.install "share/man/man1/gitc.1"
+    bin.install "bin/git-speed"
+    man1.install "share/man/man1/git-speed.1"
   end
 
   test do
-    system "#{bin}/gitc", "--version"
+    system "#{bin}/git-speed", "--version"
   end
 end
